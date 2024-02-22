3c01ce2e03b048c6ff9d38721d1bf8459a56a4f9
izzyconner <32255130+izzyconner@users.noreply.github.com>
2024-02-22 01:47:46 -0500

    Some reorg; new art assets

---
+++ MixologyConversations/Assets/SpawnOnClick.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // Populate ingredient in the mixing card table
+
+        // OR
+
+        // Drag and drop ability, instantiate a 'draggable ingredient' prefab, call the 'populateIngredient' function?
+    }
+}
