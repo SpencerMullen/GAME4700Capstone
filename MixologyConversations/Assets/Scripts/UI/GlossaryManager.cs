0b03a7ed6080df0fe436d04eb9d4b97428dab397
joey-tsai <joeytsai712@gmail.com>
2024-04-24 17:16:08 -0400

    Functional Menu
    Fully FUnctional just needs styling

--- MixologyConversations/Assets/Scripts/UI/GlossaryManager.cs
+++ MixologyConversations/Assets/Scripts/UI/GlossaryManager.cs
@@ -1,54 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlossaryManager : MonoBehaviour
 {
-    public GameObject GlossaryMenu;
+    public GameObject BookMenu;
+    public GameObject CurrentMenu;
     public GameObject GlossaryButton;
     private bool menuActivated;   
-    public GameObject CurrentMenu;
+    public Image bookImage;
+
     // Start is called before the first frame update
     void Start()
     {
         menuActivated = false;
+        bookImage = this.gameObject.GetComponent<Image>();
     }
 
-    public void OpenMenu()
+    public void handleBookMenu()
     {
-        if (GlossaryMenu != null)
+        // Book Menu doesn't exists
+        if (BookMenu == null)
         {
-            if (menuActivated)
-            {
-                Time.timeScale = 1;
-                GlossaryMenu.SetActive(false);
-                menuActivated = false;
-            }
-            else if (!menuActivated)
-            {
-                Time.timeScale = 0;
-                GlossaryMenu.SetActive(true);
-                menuActivated = true;
-            }
+            Debug.Log("Book Menu Null");
+            return;
+        }
+
+        // Handles Opening Menu
+        if (!menuActivated)
+        {
+            Time.timeScale = 0;
+            BookMenu.SetActive(true);
+            menuActivated = true;
+            var tempColor = bookImage.color;
+            tempColor.a = 1f;
+            bookImage.color = tempColor;
+            return;
+        }
+
+        // Handles Closing Menu
+        if (CurrentMenu == null)
+        {
+            Time.timeScale = 1;
+            BookMenu.SetActive(false);
+            menuActivated = false;
+            var tempColor = bookImage.color;
+            tempColor.a = 0f;
+            bookImage.color = tempColor;
+            return;
+        }
+
+        // Handles Closing Ingredient or Recipe Menu
+        if (menuActivated)
+        {
+            CurrentMenu.SetActive(false);
+            CurrentMenu = null;
+            BookMenu.SetActive(true);
+            return;
         }
     }
 
-    public void ChangeIngredientMenu()
+    public void OpenIngredientMenu()
     {
         if (menuActivated) 
         {
             CurrentMenu = transform.Find("IngredientMenu").gameObject;
-            GlossaryMenu.SetActive(false);
+            BookMenu.SetActive(false);
             CurrentMenu.SetActive(true);
         }
     }
 
-    public void ChangeRecipeMenu()
+    public void OpenRecipeMenu()
     {
         if (menuActivated) 
         {
             CurrentMenu = transform.Find("RecipeMenu").gameObject;
-            GlossaryMenu.SetActive(false);
+            BookMenu.SetActive(false);
             CurrentMenu.SetActive(true);
         }
     }
